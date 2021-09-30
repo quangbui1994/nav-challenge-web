12f63c233d7f35c3fd3bf861fad5d670dfef4856
Quang Bui <buiphuquang2412@gmail.com>
2021-10-01 00:47:15 +0300

    Change serving static files path

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import serverRenderer from './serverRenderer';
+import path from 'path';
 
 const app = express();
 const router = express.Router();
 router.get('*', serverRenderer);
 
-app.use(express.static('build'));
+app.use('/static', express.static(path.join(__dirname, 'build')));
 app.use(router)
 
 const PORT = process.env.PORT || 5000;

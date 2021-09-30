6095623a361f191d0eddecfb203e8d41f1a292dd
Quang Bui <buiphuquang2412@gmail.com>
2021-10-01 01:53:26 +0300

    Serve new HTML

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import serverRenderer from './serverRenderer';
-import path from 'path';
 
 const app = express();
 const router = express.Router();
 router.get('*', serverRenderer);
 
-app.use('/static', express.static(path.join(__dirname, 'build')));
+app.use(express.static('build'));
 app.use(router)
 
 const PORT = process.env.PORT || 5000;
